e505d9523a66c0d2ce8dd517903b3cef8513ed18
Rocio Batres <jonasgirl21@gmail.com>
2020-09-14 20:13:53 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,6 @@
 def reverse_each_word(sentence)
   "sentence".split("'")
   reversed_sentence = []
-  reversed_sentence << sentence
+  reversed_sentence << sentence.reverse
   reversed_sentence.each { |word| word.reverse } 
-  reversed_sentence
 end   
